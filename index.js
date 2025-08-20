b9bb575f388528add2cd0c99a6d57e00d0a1d4f1
Hammadnaseem109 <hammadnaseem000222@gmail.com>
2025-08-21 02:57:04 +0500

    busic Structure

---
+++ index.js
@@ -0,0 +1,112 @@
+const addnote = document.getElementById("addnote")
+const mnotediv = document.getElementById("mnotediv")
+const addedbtn = document.getElementById("addedbtn")
+const cancelbtn = document.getElementById("cancelbtn")
+const notetxt = document.getElementById("notetxt")
+const notetitle = document.getElementById("notetitle")
+const showcard = document.getElementById("showcard")
+let notelist = [];
+let firsttime = JSON.parse(localStorage.getItem("first")) ?? true;
+
+if (!firsttime) {
+    notelist = JSON.parse(localStorage.getItem("notelist")) || [];
+} else {
+    localStorage.setItem("first", JSON.stringify(false));
+    localStorage.setItem("notelist", JSON.stringify(notelist));
+}
+cancelbtn.addEventListener("click", () => {
+    mnotediv.style.display = "none"
+})
+renderingui()
+addedbtn.addEventListener("click", () => {
+    console.log("added btn click");
+    
+    mnotediv.style.display = "flex"
+    let tile = notetitle.value.trim();
+    let notee = notetxt.value.trim();
+    let note = {
+        title: tile,
+        notetext: notee,
+        complete: false
+    };
+    if (tile === "" && notee === "") {
+        alert("Fill all fields");
+    } else {
+        notelist.push(note)
+        
+        console.log(notelist);
+        localStorage.setItem("notelist", JSON.stringify(notelist))
+        notetitle.value = "";
+        notetxt.value = "";
+        mnotediv.style.display = "none";
+        renderingui()
+
+
+    }
+});
+function newnotes(){
+
+            mnotediv.style.display = "flex";
+
+}
+
+
+function renderingui() {
+    showcard.innerHTML = ""
+   if(notelist!==null){
+     notelist.forEach(element => {
+        let notecard = document.createElement("div")
+        notecard.className = "notecard"
+        notecard.id = "notecard"
+        const tilediv = document.createElement("div")
+        tilediv.className = "tilediv"
+        const textdiv = document.createElement("div")
+        textdiv.className = "textdiv"
+        const btndiv = document.createElement("div")
+        btndiv.className = "btndiv"
+        let notetile = document.createElement('h3')
+        notetile.className = "notetile"
+        notetile.id = "notetile"
+        notetile.innerText = element.title
+        let notetext = document.createElement('p')
+        notetext.classList = "notetext"
+        notetext.id = "notetext"
+        notetext.innerText = element.notetext
+        let compbtn = document.createElement('button')
+        compbtn.id = "compbtn"
+        compbtn.className = "compbtn"
+        compbtn.textContent = "Complete Text"
+        tilediv.appendChild(notetile)
+        notecard.appendChild(tilediv)
+        textdiv.appendChild(notetext)
+        btndiv.appendChild(compbtn)
+        notecard.appendChild(textdiv)
+        notecard.appendChild(btndiv)
+        showcard.appendChild(notecard)
+        if (element.complete) {
+            notecard.style.backgroundColor = "red"
+        }
+
+
+        compbtn.addEventListener('click', () => {
+            element.complete = true
+            localStorage.setItem("notelist", JSON.stringify(notelist))
+            renderingui()
+
+        })
+    });
+
+   }
+}
+function ontasklisk(){
+    window.location.href = "tasklist.html";
+
+}
+function completetask(){
+        window.location.href = "completetask.html";
+
+}
+function home(){
+        window.location.href = "index.html";
+
+}
---
+++ tasklist.js
@@ -0,0 +1,64 @@
+const showcard=document.getElementById("showcard")
+let tasklistt=[]
+tasklistt=JSON.parse(localStorage.getItem("notelist")|| null)
+console.log("tasklistt"+tasklistt);
+
+if(tasklistt===null || tasklistt.length===0){
+alert("There is not Task is here")
+}
+else{
+    renderingui()
+
+}
+function renderingui(){
+    showcard.innerHTML = ""
+tasklistt.forEach(element => {
+    if(element. complete!==true){
+        let notecard=document.createElement("div")
+    notecard.className="notecard"
+    notecard.id="notecard"
+    const tilediv=document.createElement("div")
+    tilediv.className="tilediv"
+    const textdiv=document.createElement("div")
+    textdiv.className="textdiv"
+    const btndiv=document.createElement("div")
+    btndiv.className="btndiv"
+    let notetile=document.createElement('h3')
+     notetile.className="notetile"
+     notetile.id="notetile"
+     notetile.innerText=element.title
+    let notetext=document.createElement('p')
+    notetext.classList="notetext"
+    notetext.id="notetext"
+    notetext.innerText=element.notetext
+    let  compbtn=document.createElement('button')
+    compbtn.id="compbtn"
+    compbtn.className="compbtn"
+    compbtn.textContent="Complete Text"
+    tilediv.appendChild(notetile)
+    notecard.appendChild(tilediv)
+    textdiv.appendChild(notetext)
+    notecard.appendChild(textdiv)
+    showcard.appendChild(notecard)
+
+
+    }
+      
+
+
+   
+});
+
+}
+function home(){
+    window.location.href="index.html"
+
+}
+function tasklist(){
+    window.location.href = "tasklist.html";
+
+}
+function completetask(){
+        window.location.href = "completetask.html";
+
+}
